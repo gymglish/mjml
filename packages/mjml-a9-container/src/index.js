5200cc0534921ec3a0f4b26332fca4701c7bf7fe
Aurélien Matouillot <a.matouillot@gmail.com>
2024-07-03 09:06:56 +0200

    Support template name in a9container

--- packages/mjml-a9-container/src/index.js
+++ packages/mjml-a9-container/src/index.js
@@ -16,33 +16,53 @@ export default class MjA9Container extends BodyComponent {
   `
 
   render() {
+    const templateName = this.getAttribute('template-name')
+    let headerHTML = ''
+    let footerHTML = ''
+    let trackingHTML = ''
+    let backgroundColor = ''
+    if (templateName !== 'no-background') {
+      backgroundColor = 'background-color: #F8F8F8; '
+    }
+    if (templateName !== 'partner') {
+      headerHTML = `
+      <tr>
+        <td style="padding: 20px 0px 20px 0px;">
+          <table>
+            <tr>
+              <td align="center" height="100%" style="margin: auto; padding: 0; height: 100%; vertical-align:middle;" valign="middle">
+                <img src="{{mail_logo_url}}?h=80" height="40"{% if product_obj %} alt="{{product_obj.lesson_description}}"{% endif %} />
+              </td>
+              {{PARTNERLOGO}}
+            </tr>
+          </table>
+        </td>
+      </tr>
+    `
+
+      footerHTML = `
+      <tr>
+        <td align="center" valign="middle" height="30" style="font-family: 'Open Sans', Helvetica, Arial, sans-serif; font-size: 10px; padding: 20px 0px;">
+          {{FOOTER}}
+        </td>
+      </tr>
+    `
+
+      trackingHTML = '{{TRACKING_IMAGE}}'
+    }
+
     return `
     <center>
       <table class="mobile-table" border="0" cellspacing="0" cellpadding="0" width="600" align="center"{{DIRECTION}}>
-        <tr>
-          <td style="padding: 20px 0px 20px 0px;">
-            <table>
-              <tr>
-                <td align="center" height="100%" style="margin: auto; padding: 0; height: 100%; vertical-align:middle;" valign="middle">
-                  <img src="{{mail_logo_url}}?h=80" height="40"{% if product_obj %} alt="{{product_obj.lesson_description}}"{% endif %} />
-                </td>
-                {{PARTNERLOGO}}
-              </tr>
-            </table>
-          </td>
-        </tr>
-        <tr style="background-color: #F8F8F8; font-size: 1rem;">
+        ${headerHTML}
+        <tr style="${backgroundColor}font-size: 1rem;">
           <td valign="top" style="padding: 30px 30px 30px 30px; font-family: 'Open Sans', Helvetica, Arial, sans-serif;">
             ${this.renderChildren()}
           </td>
         </tr>
-        <tr>
-          <td align="center" valign="middle" height="30" style="font-family: 'Open Sans', Helvetica, Arial, sans-serif; font-size: 10px; padding: 20px 0px;">
-            {{FOOTER}}
-          </td>
-        </tr>
+        ${footerHTML}
       </table>
-      {{TRACKING_IMAGE}}
+        ${trackingHTML}
     </center>
     `
   }
